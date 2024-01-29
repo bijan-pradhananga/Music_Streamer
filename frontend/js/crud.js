025c01a071f65db27d27e8afd5a3de3176160619
Bijan <bijan2059@gmail.com>
2024-01-29 20:08:38 +0545

    removed confirmation to delete playlist song

--- frontend/js/crud.js
+++ frontend/js/crud.js
@@ -19,18 +19,18 @@ mainSearchBar.addEventListener('keyup', function () {
 //to display all the songs
 function displaySongs() {
     fetch('phpFiles/allSongs.php')
-    .then(response => response.text())  
-    .then(data => {
-        document.querySelector('#home-content table tbody').innerHTML = data;
-    })
-    .catch(error => {
-        console.error('Error fetching data:', error);
-    });
+        .then(response => response.text())
+        .then(data => {
+            document.querySelector('#home-content table tbody').innerHTML = data;
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
 }
 
 //calling the function when the page is loaded
 displaySongs();
-document.getElementById('home-section').addEventListener('click',()=>{
+document.getElementById('home-section').addEventListener('click', () => {
     displaySongs();
 })
 
@@ -59,13 +59,13 @@ boxContents.forEach(box => {
 })
 
 //function to display liked songs
-const likedSongsDisplay = () =>  {
+const likedSongsDisplay = () => {
     fetch('phpFiles/likeSongs.php')
         .then(function (response) {
             return response.text();
         })
         .then(function (data) {
-            document.querySelector('#liked-content table tbody').innerHTML = data;  
+            document.querySelector('#liked-content table tbody').innerHTML = data;
         })
         .catch(function (error) {
             console.log(error);
@@ -73,7 +73,7 @@ const likedSongsDisplay = () =>  {
 }
 
 //defining when the above function should be called
-document.getElementById('liked-section').addEventListener('click',()=>{
+document.getElementById('liked-section').addEventListener('click', () => {
     likedSongsDisplay();
 })
 
@@ -81,7 +81,7 @@ document.getElementById('liked-section').addEventListener('click',()=>{
 function likeDislikeFn(event) {
     event.stopPropagation();
     let likeDislikeTd = event.currentTarget;
-    
+
     let songId = likeDislikeTd.querySelector('span').innerText;
     let icon = likeDislikeTd.querySelector('i');
     // Create a FormData object to send data to PHP
@@ -112,21 +112,21 @@ document.querySelectorAll('.likeDislike').forEach(elem => {
     elem.addEventListener('click', likeDislikeFn); //calling the above function on click
 });
 //to display playlist
-const displayPlaylist = () =>{
+const displayPlaylist = () => {
     fetch('phpFiles/displayPlaylist.php')
-    .then(response => response.text())  
-    .then(data => {
-        document.getElementById('playlistCollection').innerHTML = data;
-    })
-    .catch(error => {
-        console.error('Error fetching data:', error);
-    });
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('playlistCollection').innerHTML = data;
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
 }
 
 //calling the above function when site is opened
 displayPlaylist();
 //to make a playlist
-document.getElementById('playlistFormBtn').addEventListener('click', function(event) {
+document.getElementById('playlistFormBtn').addEventListener('click', function (event) {
     event.preventDefault();
     // Get form data
     var formData = new FormData(document.getElementById('playlistForm'));
@@ -135,33 +135,33 @@ document.getElementById('playlistFormBtn').addEventListener('click', function(ev
         method: 'POST',
         body: formData
     })
-    .then(response => response.text())
-    .then(data => {
-        displayPlaylist();
-        if (data.includes("Playlist Successfully Created")) {
-            document.querySelector('.popup').classList.toggle('active');
-            alert('Playlist Successfully Created');
-        }
+        .then(response => response.text())
+        .then(data => {
+            displayPlaylist();
+            if (data.includes("Playlist Successfully Created")) {
+                document.querySelector('.popup').classList.toggle('active');
+                alert('Playlist Successfully Created');
+            }
 
-        
-    })
-    .catch(error => console.error('Error:', error));
+
+        })
+        .catch(error => console.error('Error:', error));
 });
 
 //function to display playlist options
-const displayPlaylistOptions = () =>{
+const displayPlaylistOptions = () => {
     fetch('phpFiles/playlistOptions.php')
-    .then(response => response.text())  
-    .then(data => {
-        document.querySelector('#addToPlaylist-form form select').innerHTML = data;
-    })
-    .catch(error => {
-        console.error('Error fetching data:', error);
-    });
+        .then(response => response.text())
+        .then(data => {
+            document.querySelector('#addToPlaylist-form form select').innerHTML = data;
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
 }
 
 //functions to add songs to playlist
-const songIdToPlaylist =(event)=> {
+const songIdToPlaylist = (event) => {
     event.stopPropagation();
     let td = event.currentTarget;
     let songId = td.querySelector('span').innerText;
@@ -170,7 +170,7 @@ const songIdToPlaylist =(event)=> {
 }
 
 //when the button is clicked the song is added to the playlist
-document.getElementById('addToPlaylistFormBtn').addEventListener('click', function(event) {
+document.getElementById('addToPlaylistFormBtn').addEventListener('click', function (event) {
     event.preventDefault();
     // console.log('fasfasfsa');
     // Get form data
@@ -180,21 +180,21 @@ document.getElementById('addToPlaylistFormBtn').addEventListener('click', functi
         method: 'POST',
         body: formData
     })
-    .then(response => response.text())
-    .then(data => {
-        if (data.includes("Success")) {
-            document.querySelector('.popup').classList.toggle('active');
-            alert('Added to Playlist');
-        }else{
-            alert('Song is already in the playlist');
-        }
-    })
-    .catch(error => console.error('Error:', error));
+        .then(response => response.text())
+        .then(data => {
+            if (data.includes("Success")) {
+                document.querySelector('.popup').classList.toggle('active');
+                alert('Added to Playlist');
+            } else {
+                alert('Song is already in the playlist');
+            }
+        })
+        .catch(error => console.error('Error:', error));
 });
 
 //to display playlist songs
 let playlistData;
-const displayPlaylistSongs = (playlistData) =>{
+const displayPlaylistSongs = (playlistData) => {
     fetch('phpFiles/playlistSongs.php', {
         method: 'POST',
         body: playlistData
@@ -207,14 +207,14 @@ const displayPlaylistSongs = (playlistData) =>{
         })
         .catch(function (error) {
             console.log(error);
-});
+        });
 }
 
-const showPlaylistSongs =(event)=> {
-        let playlistID = event.target.id;
-        playlistData = new FormData();
-        playlistData.append('playlistID', playlistID);
-        displayPlaylistSongs(playlistData);
+const showPlaylistSongs = (event) => {
+    let playlistID = event.target.id;
+    playlistData = new FormData();
+    playlistData.append('playlistID', playlistID);
+    displayPlaylistSongs(playlistData);
 }
 
 //to delete playlist
@@ -245,26 +245,20 @@ function deletePlaylist(event) {
 //to delete playlist song
 function deletePlaylistSong(event) {
     event.stopPropagation();
-    var confirm = window.confirm('Are you sure you want to delete?')
-    if (confirm) {
-        let songId = event.target.id;
-        var formData = new FormData();
-        formData.append('Song_ID', songId);
-        fetch('phpFiles/deletePlaylistSong.php', {
-            method: 'POST',
-            body: formData
+    let songId = event.target.id;
+    var formData = new FormData();
+    formData.append('Song_ID', songId);
+    fetch('phpFiles/deletePlaylistSong.php', {
+        method: 'POST',
+        body: formData
+    })
+        .then(function (response) {
+            return response.text();
         })
-            .then(function (response) {
-                return response.text();
-            })
-            .then(function (data) {
-                if (data.includes("deleted")) {
-                    alert('deleted successfully');
-                }
-                displayPlaylistSongs(playlistData);
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-    }
+        .then(function (data) {
+            displayPlaylistSongs(playlistData);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
 }

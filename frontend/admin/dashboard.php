d81bcc2f15879cde6f053a59bb1b5df6dd7ffd99
Bijan <bijan2059@gmail.com>
2024-02-29 16:46:15 +0545

    made changes to the dashboard page

--- frontend/admin/dashboard.php
+++ frontend/admin/dashboard.php
@@ -1,10 +1,48 @@
 <?php include('header.php') ?>
 <main class="container">
-    <?php include('adminSidebar.php') ?>  
+    <?php include('adminSidebar.php') ?>
     <div class="contents">
         <div class="inner-content">
-            <h1>Dashboard</h1>
+            <h1>Welcome to Dashboard</h1>
+            <div class="status-info">
+                <div class="status-info-members" id="users">
+                    <div class="status-info-members-txt">
+                        <h3>Users</h3>
+                        <div class="count">10</div>
+                    </div>
+                    <div class="status-info-members-icon">
+                        <i class="fas fa-user"></i>
+                    </div>
+                </div>
+                <div class="status-info-members" id="artists">
+                    <div class="status-info-members-txt">
+                        <h3>Artists</h3>
+                        <div class="count">10</div>
+                    </div>
+                    <div class="status-info-members-icon">
+                        <i class="fas fa-microphone-alt"></i>
+                    </div>
+                </div>
+                <div class="status-info-members" id="songs">
+                    <div class="status-info-members-txt">
+                        <h3>Songs</h3>
+                        <div class="count">10</div>
+                    </div>
+                    <div class="status-info-members-icon">
+                        <i class="fas fa-music"></i>
+                    </div>
+                </div>
+                <div class="status-info-members" id="genres">
+                    <div class="status-info-members-txt">
+                        <h3>Genres</h3>
+                        <div class="count">10</div>
+                    </div>
+                    <div class="status-info-members-icon">
+                        <i class="far fa-play-circle"></i>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>  
+    </div>
 </main>
 <?php include('footer.php') ?>
